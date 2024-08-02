16ce4cb458fd1c05b501b541e38a5c3ce27d2ade
Ricardo Hasenack <ricardo.hasenack@fafetch.com>
2024-08-02 14:19:55 +0100

    created and populated base classes

--- classes.py
+++ classes.py
@@ -1,30 +1,46 @@
 class PBN:
+    def __str__(self):
+        return f"PBN named {self.name} with {len(self.dags)} dags"
 
-    def __init__(self, name, dag_pbn_path) -> None:
+    def __repr__(self):
+        return f"PBN named {self.name} with {len(self.dags)} dags"
+
+    def __init__(self, name, dag_pbn_path, resources_pbn_path) -> None:
         self.name = name
         self.dags = []
         self.resources = []
         self.tasks = []
         self.dag_pbn_path = dag_pbn_path
+        self.resources_pbn_path = resources_pbn_path
 
     def add_dag(self, dag):
         dag.pbn = self
         self.dags.append(dag)
 
+    def list_dags(self):
+        print(", ".join(dag.name for dag in self.dags))
+
     def add_resource(self, resource):
         resource.pbn = self
         self.resources.append(resource)
+
+    def list_resources(self):
+        print(", ".join(resource.name for resource in self.resources))
 
     def add_task(self, task):
         task.pbn = self
         self.tasks.append(task)
 
+    def list_tasks(self):
+        print(", ".join(task.name for task in self.tasks))
+
 
 class Resource:
-    def __init__(self, name) -> None:
+    def __init__(self, name, pbn, path, script_content) -> None:
         self.name = name
-        self.pbn = None
-        self.script_content = None
+        self.pbn = pbn
+        self.path = path
+        self.script_content = script_content
         self.source_tables = []
 
     def get_source_tables(self):
@@ -36,14 +52,14 @@ class Dag:
     def __init__(self, name) -> None:
         self.name = name
         self.pbn = None
-        self.table_lists = []
-        self.dataset_lists = []
-        self.project_lists = []
+        self.table_lists = {}
+        self.dataset_lists = {}
+        self.project_lists = {}
         self.tasks = []
 
     def add_task(self, task):
         task.dag = self
-        self.tasks.append()
+        self.tasks.append(task)
 
     def add_table_list(self, table_list):
         self.table_lists.append(table_list)
@@ -56,8 +72,15 @@ class Dag:
 
 
 class Task:
-    def __init__(self, name, type, parameters) -> None:
+    def __init__(self, name: str, type: str) -> None:
         self.name = name
         self.dag = None
         self.type = type
-        self.parameters = parameters
+        self.parameters = {}
+
+    def set_parameters(self, parameters: dict):
+        for key, value in parameters.items():
+            self.parameters[key] = value
+
+
+# if it's a dml script, store: dest_table, file_name, task_id, write_disposition, table_list)
--- script.py
+++ script.py
@@ -13,6 +13,8 @@ start_path = Path(
 
 ## Iterate over subfolders and create a PBN object for each one of them
 dags_folder = os.path.join(start_path, "dags")
+resources_folder = os.path.join(start_path, "resources//bq_scripts")
+
 
 PBNs = []
 resources = []
@@ -22,39 +24,84 @@ dags = []
 ## For each one of these folders, create an instance of the PBN class
 for folder in os.listdir(dags_folder):
     if folder[:9] == "data-core":
-        pbn = PBN(name=folder, dag_pbn_path=os.path.join(dags_folder, folder))
+        pbn = PBN(
+            name=folder,
+            dag_pbn_path=os.path.join(dags_folder, folder),
+            resources_pbn_path=os.path.join(resources_folder, folder),
+        )
         PBNs.append(pbn)
 
 ## Go into each of these PBNs and go over each file. Each one of them will be a DAG class instance. Add DAG to the PBN.
 for pbn in PBNs:
+    # Iterate to get DAGs and Tasks
     for filename in os.listdir(pbn.dag_pbn_path):
         if filename.endswith(".py") and "-dq" not in filename:
             dag = Dag(name=filename)
-            file = open(os.path.join(pbn.dag_pbn_path, filename))
+            file_path = os.path.join(pbn.dag_pbn_path, filename)
 
-            content = file.read()
-            print(filename)
-            table_lists = re.findall(r"table_list\s=\s*({.+?})", content, re.S)
-            dataset_lists = re.findall(r"dataset_list\s=\s*({.+?})", content, re.S)
-            project_lists = re.findall(r"project_list\s=\s*({.+?})", content, re.S)
+            with open(file_path, "r") as file:
+                tree = ast.parse(file.read(), filename=file_path)
+            variables = {}
 
-            for table_list in table_lists:
-                dag.table_lists.append(ast.literal_eval(table_list))
+            # Let's do a MVP without Datasets and Projects because that is more complicated.
+            regexp = re.compile(r"table_list(_[^,]+?\s|\s)=\s*({.+?})", re.S)
 
-            # for dataset_list in dataset_lists:
-            #     dag.dataset_lists.append(ast.literal_eval(dataset_list))
+            for node in ast.walk(tree):
+                if isinstance(node, ast.Assign):
+                    for target in node.targets:
+                        if isinstance(target, ast.Name):
+                            var_name = target.id
+                            if "table_list" in target.id:
+                                variables[var_name] = ast.literal_eval(node.value)
 
-            # for project_list in project_lists:
-            #     dag.project_lists.append(ast.literal_eval(project_list))
+                            # if it is calling a function and function name in 'run_dml_script' or 'run_raw_sql'
+                            if (
+                                isinstance(node, ast.Assign)
+                                and isinstance(node.value, ast.Call)
+                                and isinstance(node.value.func, ast.Name)
+                                and (
+                                    node.value.func.id == "run_dml_script"
+                                    or node.value.func.id == "run_raw_sql"
+                                )
+                            ):
+                                task_name = target.id
+                                task_function = node.value.func.id
+                                task = Task(name=task_name, type=task_function)
+                                # if it's a dml script, store: dest_table, file_name, task_id, write_disposition, table_list)
+                                parameters = {}
+                                # if node.value.func.id == "run_dml_script":
+                                for keyword in node.value.keywords:
+                                    if isinstance(keyword.value, ast.Name):
+                                        parameters[keyword.arg] = keyword.value.id
+                                    if isinstance(keyword.value, ast.Constant):
+                                        parameters[keyword.arg] = keyword.value.value
+                                    if isinstance(keyword.value, ast.JoinedStr):
+                                        if keyword.arg == "file_name":
+                                            for value in keyword.value.values:
+                                                if isinstance(value, ast.Constant):
+                                                    parameters[keyword.arg] = (
+                                                        value.value
+                                                    )
+                                task.set_parameters(parameters=parameters)
+                                dag.add_task(task=task)
+
+            dag.table_lists = variables
 
             dags.append(dag)
             pbn.add_dag(dag)
 
-            file.close()
+    # Iterate to get Resources
+    for filename in os.listdir(pbn.resources_pbn_path):
+        with open(os.path.join(pbn.resources_pbn_path, filename)) as f:
+            file_content = f.read()
+            resource = Resource(
+                name=filename,
+                path=os.path.join(pbn.resources_pbn_path, filename),
+                pbn=pbn,
+                script_content=file_content,
+            )
+        x = 1
+        pbn.add_resource(resource)
+        resources.append(resource)
 
 print(PBNs)
-
-
-## Also, for each of these PBNs, create the resource class. Add resource to the PBN
-
-## For each one of this DAGs, look for the relevant Tasks (run_dml_script or run_raw_sql) and create a Task instance. Add task to the DAG
